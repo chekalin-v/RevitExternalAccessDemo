51d457c798f07e241271422194e997893602eba3
Chekalin Victor <chekalin-v@yandex.ru>
2012-10-25 11:02:15 +0600

    Add copyright headers

--- src/RevitExternalAccessClient/Program.cs
+++ src/RevitExternalAccessClient/Program.cs
@@ -1,4 +1,14 @@
-﻿using System;
+﻿/* 
+ * Copyright 2012 © Victor Chekalin IVC
+ * 
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ * 
+ */
+
+using System;
 using System.ServiceModel;
 using RevitExternalAccessDemo;
 
